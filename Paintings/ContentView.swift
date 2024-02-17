289811e74d5df8f0583cd00f8d0f34236e6e122a
Пахаев Хусейн <pakhaev01@gmail.com>
2024-02-17 11:39:13 +0300

    Initial Commit

---
+++ Paintings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paintings
+//
+//  Created by Khusain on 17.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paintings/PaintingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintingsApp.swift
+//  Paintings
+//
+//  Created by Khusain on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PaintingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
